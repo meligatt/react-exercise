d5f1048fbc632bce35378f45214fd4bc917cbd7f
melissa gattoni <meligatt@gmail.com>
2019-03-09 18:11:02 +1100

    Update logic to onButtonClick handler to request data to api with input string

--- src/components/ListContainer.js
+++ src/components/ListContainer.js
@@ -8,28 +8,50 @@ import List from './List';
 // import SearchBar from './SearchBar';
 
 class ListContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      query: null,
+      warning: false
+    };
+  }
+
   onButtonClick(){
     return () => {
-      this.props.requestDataLoading();  
+      if (this.state.query === null ) { 
+        this.setState({warning: true}); 
+        return; 
+      }
+      this.props.requestDataLoading();
       axios({
         url: 'http://localhost:3000/results',
+        params: {
+          city: this.state.query
+        },
         method: 'GET'
       }).then( (response) => {
-        this.props.requestDataSuccess(response.data); 
+        this.props.requestDataSuccess(response.data);
+        this.setState({warning: false});
       }).catch((error) => {
         this.props.requestDataFailure(error); 
       });
     };
   }
+
+  onInputChange(e){
+    this.setState({query: e.target.value});
+  }
   
   render(){
+    const { warning } = this.state;
     const { items, isFetching } = this.props.properties;
     return(
       <div>
         <div style={{padding:'8px'}}>
+          <input type="text" onChange={(e) => this.onInputChange(e) }/>
           <button onClick={ this.onButtonClick() }> Search </button>
         </div>
-        <List items={ items } isFetching = { isFetching } />
+        { warning ? <div>Enter a city to search </div> : <List items={ items } isFetching = { isFetching } />}
       </div>
     );
   }
